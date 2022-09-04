444bb4a384ff3ceb07cf2bc844e682e64ea152af
ho3ein <hossein.mansouri11@gmail.com>
2022-09-05 04:22:06 +0430

    add blurhash add aspect ratio to Media model. fix in upload image in admin page. update .gitignore file.

--- apps/media/admin.py
+++ apps/media/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from apps.media.models import Media
+
 # Register your models here.
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    # list_display = ('title', 'publish', 'allow_comments', 'comment_count')
+    readonly_fields = ['blurhash', 'aspect_ratio']
--- apps/media/models.py
+++ apps/media/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+import blurhash
+from PIL import Image
+
 # Create your models here.
 
 
 class Media(models.Model):
-    url = models.URLField(max_length=200)
+    image = models.ImageField(upload_to='posts_images/')
     blurhash = models.CharField(max_length=200)
     aspect_ratio = models.FloatField()
+
+    def save(self, *args, **kwargs):
+        self.blurhash = blurhash.encode(self.image, x_components=4, y_components=3)
+
+        image = Image.open(self.image)
+        width, height = image.size
+        self.aspect_ratio = round(width / height, 1)
+        super(Media, self).save(*args, **kwargs)
