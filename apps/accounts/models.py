fe19d43f68b57c850e9c5b574299f35419b53455
ho3ein <hossein.mansouri11@gmail.com>
2022-09-25 23:08:30 +0330

    in User model: change type of avatar to Media

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 from djongo import models
 
-from apps.accounts.conf import AVATAR_DIRECTORY
 from apps.accounts.managers import UserManager
+from apps.media.models import Media
 
 
 class User(AbstractUser):
 
     bio = models.TextField(max_length=150, blank=True, null=True)
-    avatar = models.ImageField(blank=True, null=True, upload_to=AVATAR_DIRECTORY)
+    avatar = models.EmbeddedField(model_container=Media, blank=True, null=True)
     follower = models.ArrayReferenceField(to='User', on_delete=models.PROTECT, related_name='followers')
     following = models.ArrayReferenceField(to='User', on_delete=models.PROTECT, related_name='followings')
     # TODO saved_posts = models.ArrayReferenceField(to='Post', on_delete=models.PROTECT)
