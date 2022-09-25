89392fdcbbc42e299d3ab5822e14f76f955b9550
ho3ein <hossein.mansouri11@gmail.com>
2022-09-25 22:53:59 +0330

    in Media model: remove aspect_ratio field

--- apps/media/models.py
+++ apps/media/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 import blurhash
-from PIL import Image
 
 # Create your models here.
 from apps.media.conf import POSTS_IMAGES_DIRECTORY
@@ -10,12 +9,7 @@ from apps.media.conf import POSTS_IMAGES_DIRECTORY
 class Media(models.Model):
     image = models.ImageField(upload_to=POSTS_IMAGES_DIRECTORY)
     blurhash = models.CharField(max_length=200)
-    aspect_ratio = models.FloatField()
 
     def save(self, *args, **kwargs):
         self.blurhash = blurhash.encode(self.image, x_components=4, y_components=3)
-
-        image = Image.open(self.image)
-        width, height = image.size
-        self.aspect_ratio = round(width / height, 1)
         super(Media, self).save(*args, **kwargs)
